2a392d2c722fb30bf4942ff3666eae8989da8e8d
Alif Pratama <Alif Pratama@Faisal-Mualif>
2018-10-24 20:35:15 +0700

    tugas pbo

---
+++ src/program/PBO.java
@@ -0,0 +1,25 @@
+package program;
+
+
+
+import java.util.Scanner;
+
+
+public class PBO {
+  
+        @SuppressWarnings("empty-statement")
+	public static void main(String[] args){
+	int pil;
+	Scanner s= new Scanner (System.in);
+	
+	do{
+		System.out.println("1. Input data");
+		System.out.println("2. Tampil data");
+		System.out.println("3. Keluar");
+		System.out.print("Pilih : ");pil=s.nextInt();
+	}
+	while (pil != 3);
+	}
+
+    
+}
